ee1d5e5b64f4ad3c414e7e87335f0a563d798882
Srikanth Phalgun Pyaraka <36931190+PyarakaSrikanth@users.noreply.github.com>
2023-08-19 22:50:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from albumentations.pytorch import ToTensorV2
 DATASET = 'PASCAL_VOC'
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 # seed_everything()  # If you want deterministic behavior
-NUM_WORKERS = 2
+NUM_WORKERS = 0
 BATCH_SIZE = 16
 IMAGE_SIZE = 416
 NUM_CLASSES = 20
