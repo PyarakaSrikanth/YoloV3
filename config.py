f36d38bb89a2fcdc55eccd113e1ecd48f25228ff
Srikanth Phalgun Pyaraka <36931190+PyarakaSrikanth@users.noreply.github.com>
2023-08-20 13:16:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import torch
 import os
 
 from albumentations.pytorch import ToTensorV2
-
+dataset = '/kaggle/input/pascal-voc-dataset-used-in-yolov3-video/PASCAL_VOC'
 DATASET = 'PASCAL_VOC'
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 # seed_everything()  # If you want deterministic behavior
